572caaf9d92ba65412d8bc95984e3c52ac545eef
juan diaz <125918128+relampajo@users.noreply.github.com>
2023-10-04 15:59:56 -0500

    Add files via upload

---
+++ taller de variable.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+string res
+Console.WriteLine("Sabes nadar");
+res = Console.ReadLine();
+Console.WriteLine(res + " sabe nadar");
+
+Console.WriteLine("tiene igual de color favorito");
+res = Console.ReadLine();
+Console.WriteLine(res + " color favorito");
+
+Console.WriteLine("no le gusta el futbol");
+res = Console.ReadLine();
+Console.WriteLine(res + " no le gusta el futbol");
+
+Console.WriteLine("le gusta hacer deporte");
+res = Console.ReadLine();
+Console.WriteLine(res + "le gusta hacer deporte");
+
+Console.WriteLine("nacio en el mismo mes que usted");
+res = Console.ReadLine();
+Console.WriteLine(res + " en el mismo mes que usted");
+
+Console.WriteLine("le gusta los buñuelos");
+res = Console.ReadLine();
+Console.WriteLine(res + "le gusta los buñuelos");
+
+Console.WriteLine("tiene igual pasa tiempo");
+res = Console.ReadLine();
+Console.WriteLine(res + "tiene igual pasa tiempo");
+
+Console.WriteLine("le gusta los niños");
+res = Console.ReadLine();
+Console.WriteLine(res + "le gusta los niños");
+
+Console.WriteLine("le gusta las matematicas");
+res = Console.ReadLine();
+Console.WriteLine(res + "le gusta las matematicas");
+
+Console.WriteLine("tiene ojos oscuros");
+res = Console.ReadLine();
+Console.WriteLine(res + "tiene ojos oscuros");
+
+Console.WriteLine("le gusta el baile");
+res = Console.ReadLine();
+Console.WriteLine(res + "le gusta el baile");
+
+Console.WriteLine("el hijo unico");
+res = Console.ReadLine();
+Console.WriteLine(res + "el hijo unico");
+
+Console.WriteLine("le gusta la natilla");
+res = Console.ReadLine();
+Console.WriteLine(res + "le gusta la natilla");
+
+Console.WriteLine("le gusta leer");
+res = Console.ReadLine();
+Console.WriteLine(res + "le gusta leer");
+
+Console.WriteLine("tiene familia en otra cuidad");
+res = Console.ReadLine();
+Console.WriteLine(res + "tiene familia en otra cuidad");
+
+Console.WriteLine("le recebi un abrazo");
+res = Console.ReadLine();
+Console.WriteLine(res + "le recebi un abrazo");
+
+Console.WriteLine("le gusta investigar");
+res = Console.ReadLine();
+Console.WriteLine(res + "le gusta investigar");
+
+Console.WriteLine("no es de su mismo sexo");
+res = Console.ReadLine();
+Console.WriteLine(res + "no es de su mismo sexo");
+
+Console.WriteLine("se ha ganado algo");
+res = Console.ReadLine();
+Console.WriteLine(res + "se ha ganado algo");
+
+Console.WriteLine("tiene un hijo varon menor en 18 años");
+res = Console.ReadLine();
+Console.WriteLine(res + "tiene un hijo varon menor en 18 años");
